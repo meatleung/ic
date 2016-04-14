c9f1466791c35495dc037b47bff5c65dabef9016
meatleung <meatleung@gmail.com>
2016-04-14 17:34:42 +0800

    Initial commit

---
+++ app/src/main/java/com/sixinwei/ic/MainActivity.java
@@ -0,0 +1,11 @@
+package com.sixinwei.ic;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+
+public class MainActivity extends FragmentActivity
+    implements {
+
+}
---
+++ app/src/main/java/com/sixinwei/ic/SearchFragment.java
@@ -0,0 +1,7 @@
+package com.sixinwei.ic;
+
+/**
+ * Created by John on 2016-04-14.
+ */
+public class SearchFragment {
+}
